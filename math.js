179db0a1911c6e5e97d194821e19420f0dad58e3
natali-lapteva <nataliburt@gmail.ru>
2014-11-08 18:19:04 +0500

    Сделана математика во всех задачах.

--- math.js
+++ math.js
@@ -36,6 +36,18 @@ function getValue(matrixName, row, column) {
 }
 
 /**
+ * Получение значения из ячейки таблицы
+ *
+ * @param {String} matrixName название таблицы
+ * @param {Number} row строка
+ * @param {Number} column столбец
+ * @returns {Number} значение из ячейки
+ */
+function getFloatValue(matrixName, row, column) {
+    return parseFloat($('#' + matrixName + '_' + row + '_' + column).val());
+}
+
+/**
  * Установка значения в ячейку таблицы
  *
  * @param {String} matrixName название таблицы
@@ -45,4 +57,32 @@ function getValue(matrixName, row, column) {
  */
 function setValue(matrixName, row, column, value) {
     $('#' + matrixName + '_' + row + '_' + column).val(value);
+}
+
+/**
+ * Получение единичного вектора размерности n
+ *
+ * @param {Number} n
+ * @returns {Array}
+ */
+function getEVector(n) {
+    var vector = [],
+        sum = 0,
+        i;
+
+    // Задаем случайные значения вектора
+    for (i = 0; i < n; i++) {
+        sum += vector[i] = Math.random();
+    }
+
+    // Нормируем вектор
+    var sumFloor = 0;
+    for (i = 0; i < n-1; i++) {
+        sumFloor += vector[i] = Math.floor(vector[i] / sum * 10) / 10;
+    }
+
+    // В последний элемент скидываем все, что осталось от округления
+    vector[n-1] = Math.round((1 - sumFloor) * 10) / 10;
+
+    return vector;
 }
